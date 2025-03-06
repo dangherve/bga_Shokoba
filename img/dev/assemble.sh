f559804b0df58fe879e0b2ea1b6854cb4a0fa24f
dangherve <dangherve@gmail.com>
2025-03-06 17:53:40 -0500

    UI: update -small image -deck update -show active player

--- img/dev/assemble.sh
+++ img/dev/assemble.sh
@@ -1,8 +1,18 @@
 #/bin/bash
 
-convert saphir/1_r.png saphir/2_r.png saphir/3_r.png saphir/4_r.png saphir/5_r.png saphir/6_r.png dos_r.png saphir/8_r.png saphir/9_r.png saphir/10_r.png +append saphir.png
-convert rubis/1_r.png rubis/2_r.png rubis/3_r.png rubis/4_r.png rubis/5_r.png rubis/6_r.png dos_r.png rubis/8_r.png rubis/9_r.png rubis/10_r.png +append rubis.png
-convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png e_7_r.png dos_r.png dos_r.png dos_r.png  +append emeraude.png
-convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png d_7_r.png dos_r.png dos_r.png dos_r.png  +append diamond.png
+#100 x 100
+convert saphir/1.png saphir/2.png saphir/3.png saphir/4.png saphir/5.png saphir/6.png dos.png saphir/8.png saphir/9.png saphir/10.png +append saphir.png
+convert rubis/1.png rubis/2.png rubis/3.png rubis/4.png rubis/5.png rubis/6.png dos.png rubis/8.png rubis/9.png rubis/10.png +append rubis.png
+convert dos.png dos.png dos.png dos.png dos.png dos.png e_7.png dos.png dos.png dos.png  +append emeraude.png
+convert dos.png dos.png dos.png dos.png dos.png dos.png d_7.png dos.png dos.png dos.png  +append diamond.png
 
-convert saphir.png rubis.png emeraude.png diamond.png -append ../dev_cards.png
+convert saphir.png rubis.png emeraude.png diamond.png -append ../shokoba_1.png
+
+
+#250 x 250
+convert saphir/1_r.png saphir/2_r.png saphir/3_r.png saphir/4_r.png saphir/5_r.png saphir/6_r.png dos_r.png saphir/8_r.png saphir/9_r.png saphir/10_r.png +append saphir_r.png
+convert rubis/1_r.png rubis/2_r.png rubis/3_r.png rubis/4_r.png rubis/5_r.png rubis/6_r.png dos_r.png rubis/8_r.png rubis/9_r.png rubis/10_r.png +append rubis_r.png
+convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png e_7_r.png dos_r.png dos_r.png dos_r.png  +append emeraude_r.png
+convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png d_7_r.png dos_r.png dos_r.png dos_r.png  +append diamond_r.png
+
+convert saphir_r.png rubis_r.png emeraude_r.png diamond_r.png -append ../shokoba_2.png
--- img/official/assemble.sh
+++ img/official/assemble.sh
@@ -1,8 +1,18 @@
 #/bin/bash
 
-convert saphir/1_r.png saphir/2_r.png saphir/3_r.png saphir/4_r.png saphir/5_r.png saphir/6_r.png dos_r.png saphir/8_r.png saphir/9_r.png saphir/10_r.png +append saphir.png
-convert rubis/1_r.png rubis/2_r.png rubis/3_r.png rubis/4_r.png rubis/5_r.png rubis/6_r.png dos_r.png rubis/8_r.png rubis/9_r.png rubis/10_r.png +append rubis.png
-convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png e_7_r.png dos_r.png dos_r.png dos_r.png  +append emeraude.png
-convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png d_7_r.png dos_r.png dos_r.png dos_r.png  +append diamond.png
+#100 x 100
+convert saphir/1_r2.png saphir/2_r2.png saphir/3_r2.png saphir/4_r2.png saphir/5_r2.png saphir/6_r2.png dos_r2.png saphir/8_r2.png saphir/9_r2.png saphir/10_r2.png +append saphir_r2.png
+convert rubis/1_r2.png rubis/2_r2.png rubis/3_r2.png rubis/4_r2.png rubis/5_r2.png rubis/6_r2.png dos_r2.png rubis/8_r2.png rubis/9_r2.png rubis/10_r2.png +append rubis_r2.png
+convert dos_r2.png dos_r2.png dos_r2.png dos_r2.png dos_r2.png dos_r2.png e_7_r2.png dos_r2.png dos_r2.png dos_r2.png  +append emeraude_r2.png
+convert dos_r2.png dos_r2.png dos_r2.png dos_r2.png dos_r2.png dos_r2.png d_7_r2.png dos_r2.png dos_r2.png dos_r2.png  +append diamond_r2.png
 
-convert saphir.png rubis.png emeraude.png diamond.png -append ../cards.png
+convert saphir_r2.png rubis_r2.png emeraude_r2.png diamond_r2.png -append ../shokoba_3.png
+
+
+#250 x 250
+convert saphir/1_r.png saphir/2_r.png saphir/3_r.png saphir/4_r.png saphir/5_r.png saphir/6_r.png dos_r.png saphir/8_r.png saphir/9_r.png saphir/10_r.png +append saphir_r.png
+convert rubis/1_r.png rubis/2_r.png rubis/3_r.png rubis/4_r.png rubis/5_r.png rubis/6_r.png dos_r.png rubis/8_r.png rubis/9_r.png rubis/10_r.png +append rubis_r.png
+convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png e_7_r.png dos_r.png dos_r.png dos_r.png  +append emeraude_r.png
+convert dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png dos_r.png d_7_r.png dos_r.png dos_r.png dos_r.png  +append diamond_r.png
+
+convert saphir_r.png rubis_r.png emeraude_r.png diamond_r.png -append ../shokoba_4.png
